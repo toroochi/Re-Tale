bdbde9cf901bf0df840c24246bae53e219e333f0
toroochi <kanon.0982@gmail.com>
2024-01-28 00:56:50 +0900

    add : header追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React, { FunctionComponent } from 'react';
-import Button, { ButtonThemes } from './components/parts/button';
+import Button, { ButtonThemes } from './components/parts/button/button';
+import Header from './components/parts/header/header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App: FunctionComponent = () => (
-  <body>
-    <Button theme={ButtonThemes.BOX}>投稿する</Button>
-    <Button theme={ButtonThemes.CIRCLE}>a</Button>
-  </body>
+  <Router>
+    <body>
+      <Header />
+      <Button theme={ButtonThemes.BOX}>投稿する</Button>
+      <Button theme={ButtonThemes.CIRCLE}>こん</Button>
+    </body>
+  </Router>
 );
 
 export default App;
---
+++ src/components/parts/header/header.tsx
@@ -0,0 +1,34 @@
+import React, { FunctionComponent } from 'react';
+import { Link } from "react-router-dom";
+import './style.css';
+
+const Header: FunctionComponent = () => {
+    return (
+        <header className="header">
+        <Link to="/" className="logo">
+            Re:Tale
+            </Link>
+            <nav>
+            <ul className="nav-links">
+                <li className="nav-links--item">
+                <Link to="/">ホーム</Link>
+                </li>
+                <li className="nav-links--item">
+                <Link to="/info">概要</Link>
+                </li>
+                <li className="nav-links--item">
+                <Link to="/writting">執筆</Link>
+                </li>
+                <li className="nav-links--item">
+                <Link to="/user">ユーザー</Link>
+                </li>
+                <li className="nav-links--item">
+                <Link to="/">バグ報告</Link>
+                </li>
+            </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
